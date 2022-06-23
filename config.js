be182e73f92786857e1f82413c7919a1912a50ef
BaconXT <93720121+BaconXT@users.noreply.github.com>
2022-06-23 11:13:30 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
-  //Cole sua Configuração do Firebase aqui
-  
+const firebaseConfig = {
+  apiKey: "AIzaSyAOtCxdmb-oE9s3iEw-3368nJivfmc0mWA",
+  authDomain: "cicrista71.firebaseapp.com",
+  projectId: "cicrista71",
+  storageBucket: "cicrista71.appspot.com",
+  messagingSenderId: "170056501624",
+  appId: "1:170056501624:web:905e5894817f8be8f0608d"
 };
 
 firebase.initializeApp(firebaseConfig);
